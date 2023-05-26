6393737218faa885d56b260f28c20dc237bf41bc
nickjg1 <ngyorgypal@gmail.com>
2023-05-25 22:59:39 -0400

    Updated expect script

--- roles/containers/tasks/install.sh
+++ roles/containers/tasks/install.sh
@@ -17,17 +17,3 @@ sed -i '$ d' $path2
 echo "ansible-nas ansible_connection=local ansible_host=localhost" >> $path2
 ansible-galaxy install -r requirements.yml
 
-#Creates expect script to run ansible-playbook with password
-expect_script=$(cat << 'EOD'
-#!/usr/bin/expect -f
-set password [lindex $argv 0]
-spawn ansible-playbook -i inventories/my-ansible-nas/inventory nas.yml -b -K
-expect "BECOME password:"
-send "$password\r"
-interact
-EOD
-)
-cd /home/$1/ansible-nas
-sudo echo "$expect_script" > expect_script.exp
-sudo chmod +x expect_script.exp
-
